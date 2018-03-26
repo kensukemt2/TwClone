86a5e0b438ecadd9dc1ca3934b8e64c7a1fc4e3e
kensukemt2 <kensukemt2@gmail.com>
2018-03-26 20:18:29 +0900

    validate change

--- app/models/tw_clone.rb
+++ app/models/tw_clone.rb
@@ -1,3 +1,13 @@
 class TwClone < ApplicationRecord
-  validates :content, presence: true, length: {maximum: 140, message: "１４０文字以内で入力してください"}
+  #validates :content, presence: true, length: {maximum: 140, message: "１４０文字以内で入力してください"}
+  validates :tw_error
+
+  def tw_error
+    if content.blank?
+      errors[:base] << "つぶやきは空ではできません！！"
+    end
+    if content.length >140
+      errors[:base] << "１４０文字以内で入力してください"
+    end
+  end
 end
