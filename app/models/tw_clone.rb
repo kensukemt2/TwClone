27c51e257d1849601bf7cd76432f9f7465175fd8
kensukemt2 <kensukemt2@gmail.com>
2018-03-26 21:18:00 +0900

    validate

--- app/models/tw_clone.rb
+++ app/models/tw_clone.rb
@@ -1,4 +1,4 @@
 class TwClone < ApplicationRecord
-  validates :content, length: {minimum: 0, maximum: 140, message: "空では登録できません。１４０文字以内で入力してください"}
+  validates :content, length: {minimum: 1, maximum: 140, message: "空では登録できません。１４０文字以内で入力してください"}
 
 end
